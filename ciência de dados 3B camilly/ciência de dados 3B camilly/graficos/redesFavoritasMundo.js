d5e97951ba00e1fde6e0036f5fc9e88b757dc0db
camilly côrtes <127990541+CamillyCortes@users.noreply.github.com>
2024-11-25 15:07:49 -0300

    Add files via upload

---
+++ ciência de dados 3B camilly/ciência de dados 3B camilly/graficos/redesFavoritasMundo.js
@@ -0,0 +1,54 @@
+import { getCSS } from "./common.js"
+
+async function redesFavoritasMundo() {
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/redes-favoritas.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    console.log(dados);
+    const redes = Object.keys(dados)
+    const valores = Object.velues(dados)
+
+
+    const data = [
+        {
+            values: valores,
+            labels: redes,
+            type: 'pie',
+            textinfo: 'label+percent'
+        }
+
+    ]
+    const layout = {
+        plot_bgcolor: getCSS('--bg-color'),
+        paper_bgcolor: getCSS('--bg-color'),
+        title: {
+            text: 'Redes sociais com mais usuários no mundo',
+            x: 0,
+            font: {
+                color: getCSS('--primary-color'),
+                family: getCSS('--font'),
+                size: 30
+            }
+        },
+        xaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'nome das redes sociais',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        },
+        yaxis: {
+            tickfont: tickConfig,
+            title: {
+                text: 'bilhões de usuários ativos',
+                font: {
+                    color: getCSS('--secondary-color')
+                }
+            }
+        }
+    }
+}
+
+redesFavoritasMundo()
